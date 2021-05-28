6fcb98c63573c55e1d4f65b6cb4251b4d5a03cbd
srameshbabu7 <srameshbabu7@US-PF0Y9BC8>
2021-05-28 17:06:38 +0530

    "First commit"

---
+++ samplemaven/src/main/java/com/example/samplemaven/SamplemavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplemaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplemavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplemavenApplication.class, args);
+	}
+
+}
---
+++ samplemaven/src/test/java/com/example/samplemaven/SamplemavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplemaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplemavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
